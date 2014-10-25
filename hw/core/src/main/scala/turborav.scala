8d80e1afa187d5603647b10de8f185132dc37646
Joakim Andersson <joerchan@gmail.com>
2014-10-25 23:53:18 +0200

    Allow for calling sbt "last compile:run"

--- hw/core/src/main/scala/turborav.scala
+++ hw/core/src/main/scala/turborav.scala
@@ -4,8 +4,13 @@ import Chisel._
 
 object TurboRav {
   def main(args: Array[String]): Unit = {
+
+    val module = if (args.length > 0) args(0) else "cache"
+    if (args.length >= 1) args + "--backend c" + "--genHarness"
+
     val mainArgs = args.slice(1, args.length)
-    val res = args(0) match {
+
+    val res = module match {
       case "alu"      => chiselMain(mainArgs, () => Module(new Alu(32)))
       case "regbank"  => chiselMain(mainArgs, () => Module(new RegBank(32)))
       case "mult"     => chiselMain(mainArgs, () => Module(new Mult(32)))
