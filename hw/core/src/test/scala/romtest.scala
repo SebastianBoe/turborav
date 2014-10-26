e84baf9e8c214faaf6ea8f885abe0c31513d2592
Sebastian Bøe <sebastianbooe@gmail.com>
2014-10-26 20:15:10 +0100

    Rom passes basic sanity test. Not convinced the test is correct yet however.

--- hw/core/src/test/scala/romtest.scala
+++ hw/core/src/test/scala/romtest.scala
@@ -7,6 +7,16 @@ class RomTest(c: Rom, conf: TurboravConfig) extends Tester(c) {
   val HI = 1
   val LO = 0
 
+  // Convienience function
+  def expect_(
+    enable : BigInt,
+    ready  : BigInt,
+    rdata  : BigInt) = {
+    expect(c.io.enable, enable)
+    expect(c.io.ready, ready)
+    expect(c.io.rdata, rdata)
+  }
+
   // Sanity test. start with idling, then read a value from the rom.
   poke(c.io.addr, LO)
   poke(c.io.sel, LO)
@@ -16,4 +26,25 @@ class RomTest(c: Rom, conf: TurboravConfig) extends Tester(c) {
   expect(c.io.ready, LO)
   expect(c.io.rdata, LO)
   step(1)
+  // Enter setup state. (See APB spec.)
+  poke(c.io.sel, HI)
+  expect_(
+    enable = LO,
+    ready  = LO,
+    rdata  = LO
+  )
+  step(2)
+  expect_(
+    enable = HI,
+    ready  = 1,
+    rdata  = 1
+  )
+  poke(c.io.sel, LO)
+  step(1)
+  expect_(
+    enable = 0,
+    ready  = 0,
+    rdata  = 0
+  )
+
 }
