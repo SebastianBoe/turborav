de43549bc95f8aba3998483e2320afda0ad06dc2
Terje Runde <terje.runde@gmail.com>
2014-10-27 21:05:48 +0100

    Removed unused code/comments and fixed indentation.

--- hw/core/src/main/scala/cache.scala
+++ hw/core/src/main/scala/cache.scala
@@ -6,68 +6,59 @@ class Cache(val cacheLineWidth: Int,
   val numEntries: Int,
   val associativity: Int) extends Module with Constants {
 
-  val io = new Bundle {
-    val address = UInt(INPUT, 32)
-    val data    = UInt(OUTPUT, 32)
-  }
-
-  val INSTRUCTION_WIDTH_UINT = UInt(INSTRUCTION_WIDTH)
-
-  require(isPow2(numEntries))
-  require(numEntries % associativity == 0)
-  require(cacheLineWidth % INSTRUCTION_WIDTH == 0)
-
-  val entriesPerBank = numEntries / associativity
-  val instrPerCacheLine = cacheLineWidth / INSTRUCTION_WIDTH
-
-  val dataBanks = Array.fill(associativity) {
-    Mem(UInt(width = cacheLineWidth), entriesPerBank)
-  }
-
-  val numOffsetBits = log2Down(cacheLineWidth / INSTRUCTION_WIDTH)
-  val numIndexBits  = log2Down(entriesPerBank)
-  val numTagBits    = INSTRUCTION_WIDTH - numOffsetBits - numIndexBits
-
-  val tagBanks = Array.fill(associativity) {
-    new Bundle {
-      val isValid = Mem(Bool(false),              entriesPerBank)
-      val bank    = Mem(UInt(width = numTagBits), entriesPerBank)
+    val io = new Bundle {
+      val address = UInt(INPUT, 32)
+      val data    = UInt(OUTPUT, 32)
     }
-  }
 
-  val instrOffset = io.address(numOffsetBits - 1, 0)
-  val instrIndex  = io.address(numOffsetBits + numIndexBits - 1, numOffsetBits)
-  val instrTag    = io.address(numOffsetBits + numIndexBits + numTagBits - 1,
-    numOffsetBits + numIndexBits)
+    val INSTRUCTION_WIDTH_UINT = UInt(INSTRUCTION_WIDTH)
 
-  when (tagBanks(0).bank(instrIndex) === instrTag
-    && tagBanks(0).isValid(instrIndex)) {
-      //val upperInstrBitIndex = (instrOffset + UInt(1)) * INSTRUCTION_WIDTH_UINT- UInt(1)
-      //val lowerInstrBitIndex = instrOffset * INSTRUCTION_WIDTH_UINT
-      //io.data := dataBanks(0)(instrIndex)(upperInstrBitIndex, lowerInstrBitIndex)
+    require(isPow2(numEntries))
+    require(numEntries % associativity == 0)
+    require(cacheLineWidth % INSTRUCTION_WIDTH == 0)
 
-      // Create a muxer to select the bits at correct offset
-      //var muxArray = new Array[(UInt, UInt)](instrPerCacheLine)
-      //(0, INSTRUCTION_WIDTH)
+    val entriesPerBank = numEntries / associativity
+    val instrPerCacheLine = cacheLineWidth / INSTRUCTION_WIDTH
 
+    val dataBanks = Array.fill(associativity) {
+      Mem(UInt(width = cacheLineWidth), entriesPerBank)
+    }
+
+    val numOffsetBits = log2Down(cacheLineWidth / INSTRUCTION_WIDTH)
+    val numIndexBits  = log2Down(entriesPerBank)
+    val numTagBits    = INSTRUCTION_WIDTH - numOffsetBits - numIndexBits
+
+    val tagBanks = Array.fill(associativity) {
+      new Bundle {
+        val isValid = Mem(Bool(false),              entriesPerBank)
+        val bank    = Mem(UInt(width = numTagBits), entriesPerBank)
+      }
+    }
+
+    val instrOffset = io.address(numOffsetBits - 1, 0)
+    val instrIndex  = io.address(numOffsetBits + numIndexBits - 1, numOffsetBits)
+    val instrTag    = io.address(numOffsetBits + numIndexBits + numTagBits - 1,
+      numOffsetBits + numIndexBits)
+
+    /*
+    when (tagBanks(0).bank(instrIndex) === instrTag
+      && tagBanks(0).isValid(instrIndex)) {
 
     }
+    */
+
     val muxArray = Array.ofDim[(UInt, UInt)](instrPerCacheLine)
     for (i <- 0 until instrPerCacheLine) {
       val upperInstrBitIndex = i * (INSTRUCTION_WIDTH + 1) - 1
       val lowerInstrBitIndex = i * INSTRUCTION_WIDTH - 1
       muxArray(i) = (UInt(i), dataBanks(0)(instrIndex)(upperInstrBitIndex, lowerInstrBitIndex))
     }
-    //var muxArray = Array(
-    //  UInt(0) -> dataBanks(0)(instrIndex)(31, 0),
-    //  UInt(1) -> dataBanks(0)(instrIndex)(63, 32),
-    //  UInt(2) -> dataBanks(0)(instrIndex)(95, 64),
-    //  UInt(3) -> dataBanks(0)(instrIndex)(127, 96)
-    //)
+
+
     io.data := Lookup(
       instrOffset,
       UInt(0, 32),
       muxArray
     )
 
-}
+  }
