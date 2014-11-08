aed94fd9ebff04e2196b4933f281926317983f33
Joakim Andersson <joerchan@gmail.com>
2014-11-08 22:43:45 +0100

    fixed indentation in regbank

--- hw/core/src/main/scala/regbank.scala
+++ hw/core/src/main/scala/regbank.scala
@@ -12,14 +12,14 @@ class RegBank() extends Module {
     val rs2_addr = UInt(INPUT, 5)
     val rd_addr  = UInt(INPUT, 5)
     val rd_wen   = Bool(INPUT)
-        val rd_data  = UInt(INPUT, Config.xlen)
+    val rd_data  = UInt(INPUT, Config.xlen)
 
-        val rs1_data = UInt(OUTPUT, Config.xlen)
-        val rs2_data = UInt(OUTPUT, Config.xlen)
+    val rs1_data = UInt(OUTPUT, Config.xlen)
+    val rs2_data = UInt(OUTPUT, Config.xlen)
   }
 
   /* Chisel does not support initializing memory */
-    val regs = Mem(UInt(width = Config.xlen), 32)
+  val regs = Mem(UInt(width = Config.xlen), 32)
 
   when (io.rd_wen && io.rd_addr != UInt(0)) {
     regs(io.rd_addr) := io.rd_data
