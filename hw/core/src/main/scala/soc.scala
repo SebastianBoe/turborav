d49fda5de71e6a11e9166c460dd7493d4035d0ac
Sebastian Bøe <sebastianbooe@gmail.com>
2014-12-30 18:50:58 +0100

    Started on integrating the ram with the soc.

--- hw/core/src/main/scala/soc.scala
+++ hw/core/src/main/scala/soc.scala
@@ -11,11 +11,21 @@ import Constants._
 // Chisel code, if we go any higher we venture into nasty
 // fpga-specific verilog.
 
+// Memory map
+// The two most significant bits divide the memory map into 4 parts.
+
+// Bit 31,30
+//      1, 0 // Reserved for future use.
+//      1, 1 // Reserved for future use.
+//      0, 1 // RAM memory map
+//      0, 0 // ROM memory map
+
 class Soc extends Module {
   val io = new Bundle { val stub = Bool(INPUT) }
 
   val ravv    = Module(new RavV())
   val rom     = Module(new Rom ())
+  val ram     = Module(new Ram ())
   val adapter = Module(new RRApbAdapter())
 
   ravv.io <> adapter.io.rr
