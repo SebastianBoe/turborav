aecb326ef02575c7f96a1cd51262ec1dc2270c4b
Sebastian Bøe <sebastianbooe@gmail.com>
2014-12-31 09:31:36 +0100

    Improved the memory map comments.

--- hw/core/src/main/scala/soc.scala
+++ hw/core/src/main/scala/soc.scala
@@ -12,13 +12,22 @@ import Constants._
 // fpga-specific verilog.
 
 // Memory map
-// The two most significant bits divide the memory map into 4 parts.
 
-// Bit 31,30
-//      1, 0 // Reserved for future use.
-//      1, 1 // Reserved for future use.
-//      0, 1 // RAM memory map
-//      0, 0 // ROM memory map
+// The 4 most significant bits divide the memory map into 16 segments.
+// Only the three lowest segments are defined. The lowest segment is
+// ROM, followed by RAM, and then memory mapped IO (AKA peripherals).
+
+// Bit 31,30,29,28
+//      0, 0, 0, 0 // ROM memory map
+//      0, 0, 0, 1 // RAM memory map
+//      0, 0, 1, 0 // MMIO
+//      otherwise  // Reserved for future use
+
+// See the following 32 bit addresses as an example
+// 0x0000_0000 // Rom address
+// 0x1000_0000 // Ram address
+// 0x2000_0000 // Peripheral hardware register address
+// 0x3000_0000 // Reserved
 
 class Soc extends Module {
   val io = new Bundle { val stub = Bool(INPUT) }
