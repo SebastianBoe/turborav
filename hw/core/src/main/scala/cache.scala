4f213aa7765ccc9b92ef6446af6b28f19e4549ee
Joakim Andersson <joerchan@gmail.com>
2015-01-14 21:14:43 +0100

    Passive aggresive?

--- hw/core/src/main/scala/cache.scala
+++ hw/core/src/main/scala/cache.scala
@@ -47,14 +47,7 @@ class Cache() extends Module {
   val tagHits = Vec.fill(associativity) { Bool() }
 
   /* Linear Feedback register used for random replacement policy */
-  // pro-tip: val shift = LFSR16(io.write) (Ta det som et complement
-  // at du implementerte det nesten identisk som Chisel forfatterne).
-  val shift= Reg(init = UInt(1, 16))
-  when (io.write) {
-    val bit = shift(0)^shift(2)^shift(3)^shift(5)
-    val shift_next = Cat(bit, shift(15,1))
-    shift:= shift_next
-  }
+  val shift = LFSR16(io.write)
 
   for( i <- 0 until associativity) {
     val bank = Mem(Bits(width = cacheLineWidth), entriesPerBank,seqRead = true)
