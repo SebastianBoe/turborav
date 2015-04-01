58b2db099d37b13b972ee9afffa6e4ea728a0009
Sebastian Bøe <sebastianbooe@gmail.com>
2015-04-01 15:43:43 +0200

    Increased timeout in batchprocessing.

--- hw/core/all.sh
+++ hw/core/all.sh
@@ -1,4 +1,3 @@
 for t in $(find isa/rv32ui -name *.S -exec basename {} .S \; | sort); do
-    timeout 30s make ROM=isa/rv32ui-p-${t} riscv.test
-    sleep 0.2s
+    timeout 10m make ROM=isa/rv32ui-p-${t} riscv.test
 done
