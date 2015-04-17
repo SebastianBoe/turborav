375376403bc050284c387443d754f796bee9f86f
Sebastian Bøe <sebastianbooe@gmail.com>
2015-04-17 19:06:02 +0200

    Resolved load-use hazard.
    This was a new hazard for me. Load-use hazards have always been between
    mem and exe, but this was actually a load-use hazard between mem and
    mem. See comment for when it can occur. It was discovered in the memcpy
    implementation.

--- hw/core/src/main/scala/memory.scala
+++ hw/core/src/main/scala/memory.scala
@@ -9,9 +9,6 @@ class Memory extends Module {
 
   // Pipeline registers
   val exe_mem = Reg(init = new ExecuteMemory())
-  when(!io.i_stall){
-    exe_mem := io.exe_mem
-  }
 
   val request  = io.requestResponseIo.request
   val response = io.requestResponseIo.response
@@ -27,6 +24,21 @@ class Memory extends Module {
   request.bits.write := mem_ctrl.write
 
   val ram_word = response.bits.word
+
+  when(!io.i_stall){
+    exe_mem := io.exe_mem
+
+    // In the normal load-use hazard where the exe stage is the user
+    // we need a stall no matter what (in an in-order-processor). But
+    // not when the mem stage itself is the user, like in memcpy:
+
+    // lw tp,0(sp)
+    // sw tp,0(ra)
+    //
+    // In this case we can forward to ourselves. We do this below.
+
+    exe_mem.rs2 := Mux(mem_ctrl.read, ram_word, exe_mem.rs2)
+  }
 
   io.mem_wrb.mem_read_data := ram_word
   io.mem_wrb <> exe_mem
