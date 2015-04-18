fdb4c9e31894a9f5666d6f096250e6f4b7fcf80b
hakonfam <hakon.amu@gmail.com>
2015-04-18 20:43:32 +0200

    Updated timer module to support start and reset signals.
    See comment in source file for usage.

--- hw/core/src/main/scala/timer.scala
+++ hw/core/src/main/scala/timer.scala
@@ -4,19 +4,36 @@ import Chisel._
 import Constants._
 import Common._
 
+/* 
+  in_start: Start timer
+  in_reset: Reset timer value to 0
+            NOTE: To get the timer running again you need to disable this
+                  signal.
+
+  out_val:  Used to read out current timer value
+*/ 
+
 class Timer() extends Module {
   val xlen = Config.xlen 
   require(isPow2(xlen))
 
   val io = new Bundle() {
+    val in_start  = Bool(INPUT)
+    val in_reset  = Bool(INPUT)
+
     val out_val   = UInt(OUTPUT, xlen)
   }
 
-  val state = Reg(init = UInt(0,width=xlen))
+  val state   = Reg(init = UInt(0,width=xlen))
 
-  when(Bool(true))
-  {
+  when(io.in_start) {
     state := state + UInt(1)
   }
+  
+  when(io.in_reset) {
+    state := UInt(0)
+  }
+
+
   io.out_val := state 
 }
--- hw/core/src/test/scala/timertest.scala
+++ hw/core/src/test/scala/timertest.scala
@@ -4,10 +4,36 @@ import Chisel._
 import Constants._
 
 class TimerTest(timer: Timer) extends Tester(timer) {
-  val thousand = BigInt(1000)
-  val zero = BigInt(0)
 
-  expect(timer.io.out_val, zero)
+  // Running timer without setting in_start to '1' results in no update
+  expect(timer.io.out_val, BigInt(0))
   step(1000)
-  expect(timer.io.out_val, thousand)
+  expect(timer.io.out_val, BigInt(0))
+
+  // Set in_start and expect timer to be running
+  poke(timer.io.in_start, BigInt(1))
+  step(1000)
+  expect(timer.io.out_val, BigInt(1000))
+   
+  // Set in_reset and expect timer value to be 0
+  poke(timer.io.in_reset, BigInt(1))
+  step(1)
+  expect(timer.io.out_val,BigInt(0) )
+  step(1)
+  expect(timer.io.out_val, BigInt(0))
+
+  // Unset in_reset and expect timer value to be updated again
+  poke(timer.io.in_reset, BigInt(0))
+  step(1)
+  expect(timer.io.out_val, BigInt(1))
+  step(1)
+  expect(timer.io.out_val, BigInt(2))
+
+  // Unset in_start and expect timer value to not be updated
+  poke(timer.io.in_start, BigInt(0))
+  step(1)
+  expect(timer.io.out_val, BigInt(2))
+
+
+
 }
