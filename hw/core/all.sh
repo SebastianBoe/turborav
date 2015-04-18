75db695f56ca3ae2c0c32ecad31735c415a2949f
Sebastian Bøe <sebastianbooe@gmail.com>
2015-04-18 14:57:08 +0200

    No longer running amo tests.
    Added a filter to all.sh so it doesn't execute the amo tests.

--- hw/core/all.sh
+++ hw/core/all.sh
@@ -1,3 +1,12 @@
-for t in $(find isa/rv32ui -name *.S -exec basename {} .S \; | sort); do
+find_tests(){
+    find isa/rv32ui \
+         -name *.S \
+         -exec basename \
+         {} .S \; \
+        | sort \
+        | grep --invert-match amo
+}
+
+for t in $(find_tests); do
     timeout 10m make ROM=generated/rv32ui-p-${t} riscv.test
 done
