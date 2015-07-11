f59fa3a49edc1829806334b8f76b146d6a65f7b3
Sebastian Bøe <sebastianbooe@gmail.com>
2015-07-11 14:42:22 +0200

    First attempt at encoder. Untested.

--- hw/src/main/scala/dvi_tmds_encoder.scala
+++ hw/src/main/scala/dvi_tmds_encoder.scala
@@ -6,21 +6,21 @@ import Constants._
 // This class implements the flow chart on page 29 in the DVI spec
 // V1.0. This code is impossible to understand without the spec. and
 // hard to understand with it!
+
+// The code is optimized to be obviously matching the spec. on page
+// 29, not to be obviously correct by itself.
 class dvi_tmds_encoder() extends Module {
   val io = new Bundle(){
-    val d        = UInt(INPUT, 8)
-    val c        = UInt(INPUT, 2)
-    val de = Bool(INPUT)
+    val d     = UInt(INPUT, 8)
+    val c     = UInt(INPUT, 2)
+    val de    = Bool(INPUT)
 
-    val q_out       = UInt(OUTPUT, 10)
+    val q_out = UInt(OUTPUT, 10)
   }
 
-  val cnt_prev = Reg(init = UInt(0, 3))
   val cnt      = Reg(init = UInt(0, 3))
 
-  cnt := UInt(0) // TODO
-
-  val majority = ((PopCount(io.d)  >  UInt(4))) ||
+  val majority = ((N1(io.d)  >  UInt(4))) ||
                   (N1(io.d) === N0(io.d) && (io.d(0) === UInt(0)))
 
   val q_m_vec  = Vec.fill(9){ Bool() }
@@ -34,36 +34,40 @@ class dvi_tmds_encoder() extends Module {
        (q_m_vec(i-1) ^ io.d(i))
     )
   val q_m = q_m_vec.toBits
-
+  val q_m_0_7 = q_m(7,0)
   when(io.de) {
-    when(cnt === UInt(0) || PopCount(q_m(7,0)) === UInt(4)) {
+    when(cnt === UInt(0) || N1(q_m_0_7) === N0(q_m_0_7)) {
       io.q_out := Cat(
         ~q_m(8),
          q_m(8),
-        Mux(q_m(8), q_m(7,0), ~q_m(7,0))
+        Mux(q_m(8), q_m_0_7, ~q_m_0_7)
       )
-      when(q_m(8)) {
-        cnt := cnt + (PopCount(~q_m(7,0)) - PopCount(q_m(7,0)))
+      when(q_m(8) === UInt(0)) {
+        cnt := cnt + (N0(q_m_0_7) - N1(q_m_0_7))
       }.otherwise {
-        cnt := cnt + (PopCount(q_m(7,0)) - PopCount(~q_m(7,0)))
+        cnt := cnt + (N1(q_m_0_7) - N0(q_m_0_7))
       }
     }.otherwise {
-      when(cnt > UInt(0) && PopCount(~q_m(7,0)) > PopCount(q_m(7,0))) {
+      when(
+        cnt > UInt(0) && N1(q_m_0_7) > N0(q_m_0_7)
+          ||
+        cnt < UInt(0) && N0(q_m_0_7) > N1(q_m_0_7)
+      ) {
         io.q_out := Cat(
           UInt(1),
           q_m(8),
-          ~q_m(7,0)
+          ~q_m_0_7
         )
         cnt := cnt + (q_m(8) << UInt(1)) +
-          (PopCount(~q_m(7,0)) - PopCount(q_m(7,0)))
+          (N0(q_m_0_7) - N1(q_m_0_7))
       }.otherwise {
         io.q_out := Cat(
           UInt(0),
           q_m(8),
-          q_m(7,0)
+          q_m_0_7
         )
         cnt := cnt - (~q_m(8) << UInt(1)) +
-          (PopCount(q_m(7,0)) - PopCount(~q_m(7,0)))
+          (N1(q_m_0_7) - N0(q_m_0_7))
       }
     }
   }.otherwise {
