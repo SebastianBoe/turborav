9b51c769b58a4a7a67d70ac935b479139bcef425
Sebastian Bøe <sebastianbooe@gmail.com>
2015-07-18 22:51:37 +0200

    Sanity dvi test.

--- hw/src/test/scala/dvi_tmds_encodertest.scala
+++ hw/src/test/scala/dvi_tmds_encodertest.scala
@@ -5,4 +5,12 @@ import Constants._
 
 class dvi_tmds_encoderTest(c: dvi_tmds_encoder) extends Tester(c) {
   step(1)
+  poke(c.io.d, UInt(3))
+  poke(c.io.c, UInt("0b11"))
+  poke(c.io.de, Bool(false))
+  for (i <- 0 to 3) {
+    poke(c.io.c, 
+    expect(c.
+  }
+  expect(c.io.q_out, UInt("0b1101010101"))
 }
