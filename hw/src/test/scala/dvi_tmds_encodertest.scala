ae46724af6da5b3c78312b7d135bd4f7dbc1059e
Sebastian Bøe <sebastianbooe@gmail.com>
2015-07-21 09:40:36 +0200

    Improved DVI test.

--- hw/src/test/scala/dvi_tmds_encodertest.scala
+++ hw/src/test/scala/dvi_tmds_encodertest.scala
@@ -4,13 +4,20 @@ import Chisel._
 import Constants._
 
 class dvi_tmds_encoderTest(c: dvi_tmds_encoder) extends Tester(c) {
+
   step(1)
-  poke(c.io.d, UInt(3))
-  poke(c.io.c, UInt("0b11"))
-  poke(c.io.de, Bool(false))
+  poke(c.io.d, 3)
+  poke(c.io.de, 0)
   for (i <- 0 to 3) {
-    poke(c.io.c, 
-    expect(c.
+    poke(c.io.c, i)
+    expect(
+      c.io.q_out,
+      List(
+        ScalaUtil.b("0010101011"),
+        ScalaUtil.b("1101010100"),
+        ScalaUtil.b("0010101010"),
+        ScalaUtil.b("1101010101")
+      )(i)
+    )
   }
-  expect(c.io.q_out, UInt("0b1101010101"))
 }
