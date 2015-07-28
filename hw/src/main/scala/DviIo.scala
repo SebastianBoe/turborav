d92eb3cf44bd7733252bef013e3d4bb38060bd3e
Sebastian Bøe <sebastianbooe@gmail.com>
2015-07-28 15:45:54 +0200

    Removed dvi_clk code.
    Will do this in verilog or with a black box instead perhaps.

--- hw/src/main/scala/DviIo.scala
+++ hw/src/main/scala/DviIo.scala
@@ -4,11 +4,4 @@ import Chisel._
 
 class DviIo extends Bundle {
   val chan = Vec.fill(3) {Bool()}.asOutput()
-
-  // From the DVI spec.
-  /** The T.M.D.S. clock channel carries a character-rate frequency
-   reference from which the receiver produces a bit-rate sample clock for
-   the incoming serial streams.
-   */
-  val chan_clk = Bool(OUTPUT)
 }
--- hw/src/main/scala/dvi_tmds_transmitter.scala
+++ hw/src/main/scala/dvi_tmds_transmitter.scala
@@ -17,13 +17,6 @@ class dvi_tmds_transmitter extends Module {
     val dvi_io  = new DviIo()
   }
 
-  // The system clock is assumed to be 50MHz (Although it would be
-  // possible to connect a 100MHz Oscillator). The minimum DVI clock
-  // frequency is conveniently 50MHz / 2 = 25MHz.
-  val clk_25MHz = Reg(init = Bool(false))
-  clk_25MHz := ! clk_25MHz
-  io.dvi_io.chan_clk := clk_25MHz
-
   // Create three combinatorial encoders and serialize their output
   // with a sequential serializer.
   for (i <- 0 to num_chan - 1) {
