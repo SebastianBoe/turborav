f6102d5a9ca63265e29cad6440efd60bf86f59e0
Sebastian Bøe <sebastianbooe@gmail.com>
2015-07-28 16:16:57 +0200

    Changed how serialization is done and implemented the cnt feature in the encoder.

--- hw/src/main/scala/dvi_tmds_transmitter.scala
+++ hw/src/main/scala/dvi_tmds_transmitter.scala
@@ -17,23 +17,27 @@ class dvi_tmds_transmitter extends Module {
     val dvi_io  = new DviIo()
   }
 
+  // Keeps track of which of the 10 bits we are currently
+  // transmitting. NB: This is not related to encoder.io.cnt, which
+  // counts the balance of 1's and 0's in the RGB pixels.
+  val bit_counter = Counter(cond = io.enable, n = 10)
+  val bit_counter_value = bit_counter._1
+  val bit_counter_wrap  = bit_counter._2
+
   // Create three combinatorial encoders and serialize their output
-  // with a sequential serializer.
+  // with bit_counter.
   for (i <- 0 to num_chan - 1) {
     val encoder = Module(new dvi_tmds_encoder())
     encoder.io.c  := io.ctl(i)
     encoder.io.d  := io.rgb(i)
     encoder.io.de := io.de
-    encoder.io.cnt := UInt(0) //TODO
-    val cnt_next = encoder.io.cnt_next //TODO
 
-    val s = Module(new Serializer(encoder.io.q_out.getWidth))
-    s.io.in := encoder.io.q_out
+    // The encoder computes what the next cnt value should be, and we
+    // capture that cnt value at 10-bit character boundaries.
+    val encoder_cnt = Reg(init = SInt(0, 4))
+    when(bit_counter_wrap) { encoder_cnt := encoder.io.cnt_next }
+    encoder.io.cnt := encoder_cnt
 
-    // Simply wiring it like this introduces the requirement that the
-    // enable signal must be held high until the transmission is over.
-    s.io.cond := io.enable
-
-    io.dvi_io.chan(i) := s.io.out
+    io.dvi_io.chan(i) := encoder.io.q_out(bit_counter_value)
   }
 }
