8522f468c680cbb1171be0327d83a7e84487b8e1
Sebastian Bøe <sebastianbooe@gmail.com>
2015-10-31 16:54:33 +0100

    Fixed "refactoring".

--- hw/src/main/scala/FpgaRam.scala
+++ hw/src/main/scala/FpgaRam.scala
@@ -33,15 +33,24 @@ class FpgaRam extends Module {
                                 UInt("b1111")
              ))
 
-  val write_word  = LeftRotate(io.word_w, byte_addr << UInt(3))
-  val enable_mask = LeftRotate(mask, byte_addr)
-
   val bytes_out = Vec.fill(word_size_in_bytes) { Reg(init = UInt(0))}
+
+  val word_shifted = Vec.fill(word_size_in_bytes) { UInt()}
+  val mask_shifted = Vec.fill(word_size_in_bytes) { UInt()}
+  for(i <- 0 until word_size_in_bytes){
+    word_shifted(i) := LeftRotate(io.word_w , i * 8)
+    mask_shifted(i) := LeftRotate(mask      , i    )
+  }
+
   for(i <- 0 until word_size_in_bytes){
     /* Ram is kept in RAID0 configuration for FPGA performance
        reasons. There is one ram module per byte in a word */
     val ram_stripe = Mem(UInt(width = 8), num_words)
     val ram_addr = base_addr + UInt(byte_addr > UInt(i))
+
+    val enable_mask = mask_shifted(byte_addr)
+    val write_word  = word_shifted(byte_addr)
+
     when(enable_mask(i)) {
       when(io.wen) {
         ram_stripe(ram_addr) := write_word(i * 8 + 7, i * 8)
@@ -52,5 +61,17 @@ class FpgaRam extends Module {
       bytes_out(i) := UInt(0)
     }
   }
-  io.word_r := LeftRotate(bytes_out.toBits(), Reg(byte_addr))
+
+  val word_out = bytes_out.toBits()
+  //TODO: Investigate why chisel couldn't infer this width.
+  word_out.setWidth(32)
+
+  val word_out_shifted = Vec.fill(word_size_in_bytes) { UInt()}
+  for(i <- 0 until word_size_in_bytes){
+    word_out_shifted(i) := RightRotate(word_out, i * 8)
+  }
+
+  io.word_r := word_out_shifted(
+    Reg(init = UInt(0), next = byte_addr)
+  )
 }
