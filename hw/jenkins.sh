dc61346c67187684e685b888ef64485410b83811
Sebastian Bøe <sebastianbooe@gmail.com>
2015-11-07 13:04:35 +0100

    Added Yosys output to the CI targets.

--- hw/jenkins.sh
+++ hw/jenkins.sh
@@ -2,4 +2,5 @@ source /opt/Xilinx/14.7/ISE_DS/settings64.sh
 scons -j4\
     build/test/ \
     build/synthesis/slices.txt \
-    build/synthesis/timing.txt
+    build/synthesis/timing.txt \
+    build/synthesis/Soc.blif
