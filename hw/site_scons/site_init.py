f2f977197c3b87e3b008f2f90d46d524864371dd
Sebastian Bøe <sebastianbooe@gmail.com>
2016-01-23 13:51:10 +0100

    Reduced the number of gpio pins.
    Reduced the number of GPIOs from 20 to 8 to be able to fit the Soc into
    the 8k iCE40. I have only been able to find the pin names of a few of
    the 8k iCE40 pins.

--- hw/site_scons/site_init.py
+++ hw/site_scons/site_init.py
@@ -7,8 +7,8 @@ def sim_builder_generator(source, target, env, for_signature):
     cmd += " $MODULE "
     cmd += " $TARGET_DIR "
     cmd += " $ROM "
-    cmd += " 12 "
-    cmd += " 8 "
+    cmd += " 4 "
+    cmd += " 4 "
     cmd += " False &> $TARGET_DIR/log.txt || cat $TARGET_DIR/log.txt; "
 
     cmd += " vcd2fst $TARGET_DIR/${TOP_MODULE}.vcd $TARGET_DIR/wave.fst;"
