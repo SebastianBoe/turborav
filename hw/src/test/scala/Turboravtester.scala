14610ee9014449b52b8efdef44c86d4bd2f367c8
Sebastian Bøe <sebastianbooe@gmail.com>
2016-03-16 18:19:47 +0100

    turboravtester: Made CLI more user-friendly

--- hw/src/test/scala/Turboravtester.scala
+++ hw/src/test/scala/Turboravtester.scala
@@ -3,16 +3,72 @@ package TurboRav
 import Chisel._
 import org.apache.commons.io.FilenameUtils;
 
-object TurboRavTestRunner{
-  def main(args: Array[String]): Unit = {
-    val Array( // Parse argument list
-      module,
-      target_dir,
-      rom,
-      num_pin_inputs,
-      num_pin_outputs,
-      fpga
-    ) = args
+object TurboRavTestRunner {
+
+  val default_gpio_input_pins  = 4
+  val default_gpio_output_pins = 4
+  val default_fpga             = true
+
+  val help = s"""
+Usage: scala TurboRav.TurboRavTestRunner [OPTIONS] COMMAND
+
+COMMAND:
+  See Turboravtester.scala for available commands.
+
+Options:
+  -i <int>, --gpio-input-pins <int>
+    The number of GPIO input pins to instantiate
+    Default: $default_gpio_input_pins
+
+  -o <int>, --gpio-output-pins <int>
+    The number of GPIO output pins to instantiate
+    Default: $default_gpio_output_pins
+
+  -r <file>, --rom <file>
+    Synthesize the elf file <file> into the boot ROM. The size of the
+    HW ROM will dynamically increase (at elaboration time) to fit the
+    elf program.
+
+  -t <dir>, --target-directory <dir>
+    Put generated files in <dir>.
+
+  --fpga, --no-fpga
+    Configure the build for FPGA synthesis.
+    Default: --fpga
+"""
+  def main(args: Array[String]) {
+    if (args.length == 0) println(help)
+
+    type OptionMap = Map[Symbol, Any]
+
+    def nextOption(map : OptionMap, list: List[String]) : OptionMap = {
+      list match {
+        case Nil => map
+
+        case "-i"                 :: value :: tail => nextOption(map ++ Map('i          -> value.toInt ), tail)
+        case "--gpio-input-pins"  :: value :: tail => nextOption(map ++ Map('i          -> value.toInt ), tail)
+        case "-o"                 :: value :: tail => nextOption(map ++ Map('o          -> value.toInt ), tail)
+        case "--gpio-output-pins" :: value :: tail => nextOption(map ++ Map('o          -> value.toInt ), tail)
+        case "-r"                 :: value :: tail => nextOption(map ++ Map('rom        -> value       ), tail)
+        case "--rom"              :: value :: tail => nextOption(map ++ Map('rom        -> value       ), tail)
+        case "-t"                 :: value :: tail => nextOption(map ++ Map('target_dir -> value       ), tail)
+        case "--target-directory" :: value :: tail => nextOption(map ++ Map('target_dir -> value       ), tail)
+        case "--fpga"             ::          tail => nextOption(map ++ Map('fpga       -> true        ), tail)
+        case "--no-fpga"          ::          tail => nextOption(map ++ Map('fpga       -> false       ), tail)
+        case string               :: Nil           => nextOption(map ++ Map('module     -> string      ), list.tail)
+
+        case option               ::          tail => println(help + "Unknown option " + option); sys.exit(1)
+      }
+    }
+
+    val options = nextOption(Map(), args.toList)
+
+    val target_dir      = options.get('target_dir).orNull.toString
+    val module          = options('module)
+    val rom             = options('rom).asInstanceOf[String]
+    val num_pin_inputs  = options.getOrElse('i, default_gpio_input_pins ).asInstanceOf[Int]
+    val num_pin_outputs = options.getOrElse('o, default_gpio_output_pins).asInstanceOf[Int]
+    val fpga            = options.getOrElse('fpga, default_fpga).asInstanceOf[Boolean]
 
     val test_args = Array(
       "--genHarness",
@@ -88,18 +144,18 @@ object TurboRavTestRunner{
       case "Soctest" =>
         chiselMainTest(test_args, () => Module(new Soc(
           rom,
-          num_pin_inputs.toInt,
-          num_pin_outputs.toInt,
-          fpga.toBoolean
+          num_pin_inputs,
+          num_pin_outputs,
+          fpga
         ))){
           c => new SocTest(c)
         }
       case "Riscvtest" =>
         chiselMainTest(test_args, () => Module(new Soc(
           rom,
-          num_pin_inputs.toInt,
-          num_pin_outputs.toInt,
-          fpga.toBoolean
+          num_pin_inputs,
+          num_pin_outputs,
+          fpga
         ))){
           c => new RiscvTest(c, target_dir)
         }
@@ -108,5 +164,6 @@ object TurboRavTestRunner{
           c => new FpgaRamTest(c)
         }
     }
+    //TODO: simplify scala command by doing waveform tricks here?
   }
 }
