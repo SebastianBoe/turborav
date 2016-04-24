49392540dff9d678431d6f7ff6bbdbbb3a373e80
Sebastian Bøe <sebastianbooe@gmail.com>
2016-04-24 15:12:49 +0200

    fwu: Refactor

--- hw/src/main/scala/fwu.scala
+++ hw/src/main/scala/fwu.scala
@@ -11,21 +11,35 @@ class ForwardingUnit extends Module {
 
   val io = new ForwardingUnitIO()
 
-  io.fwu_exe.rs1_sel := MuxCase( RS_SEL_DEC, Array(
-            ( io.fwu_mem.rd_wen              &&
-              io.fwu_mem.rd_addr =/= UInt(0) &&
-              io.fwu_mem.rd_addr === io.fwu_exe.rs1_addr) -> RS_SEL_MEM,
-            ( io.fwu_wrb.rd_wen              &&
-              io.fwu_wrb.rd_addr =/= UInt(0) &&
-              io.fwu_wrb.rd_addr === io.fwu_exe.rs1_addr) -> RS_SEL_WRB
-            ))
+  io.fwu_exe.rs1_sel := MuxCase(
+    RS_SEL_DEC,
+    Array(
+      All(
+        io.fwu_mem.rd_wen,
+        io.fwu_mem.rd_addr =/= UInt(0),
+        io.fwu_mem.rd_addr === io.fwu_exe.rs1_addr
+        ) -> RS_SEL_MEM,
+      All(
+        io.fwu_wrb.rd_wen,
+        io.fwu_wrb.rd_addr =/= UInt(0),
+        io.fwu_wrb.rd_addr === io.fwu_exe.rs1_addr
+      ) -> RS_SEL_WRB
+    )
+  )
 
-  io.fwu_exe.rs2_sel := MuxCase( RS_SEL_DEC, Array(
-            ( io.fwu_mem.rd_wen              &&
-              io.fwu_mem.rd_addr =/= UInt(0) &&
-              io.fwu_mem.rd_addr === io.fwu_exe.rs2_addr) -> RS_SEL_MEM,
-            ( io.fwu_wrb.rd_wen              &&
-              io.fwu_wrb.rd_addr =/= UInt(0) &&
-              io.fwu_wrb.rd_addr === io.fwu_exe.rs2_addr) -> RS_SEL_WRB
-            ))
+  io.fwu_exe.rs2_sel := MuxCase(
+    RS_SEL_DEC,
+    Array(
+      All(
+        io.fwu_mem.rd_wen,
+        io.fwu_mem.rd_addr =/= UInt(0),
+        io.fwu_mem.rd_addr === io.fwu_exe.rs2_addr
+      ) -> RS_SEL_MEM,
+      All(
+        io.fwu_wrb.rd_wen,
+        io.fwu_wrb.rd_addr =/= UInt(0),
+        io.fwu_wrb.rd_addr === io.fwu_exe.rs2_addr
+      ) -> RS_SEL_WRB
+    )
+  )
 }
