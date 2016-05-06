c4cad453d7f00c4c06201e8fc68717bf18a4cba3
Sebastian Bøe <sebastianbooe@gmail.com>
2016-05-06 12:23:34 +0200

    csmith: Added support for Csmith based testing
    I am trying to do some more extensive testing of the CPU. I had a look
    at riscv-torture, but found that it was a hassle to set up for
    RV32UI[0]. So instead I am copying Clifford Wolf's technique of
    generating C programs and verifying that the custom CPU executes these
    programs in the same way as a golden model does.
    
    Not much is supported yet;
    
    Csmith has been added as a dependency in the Dockerfile
    
    The Csmith test harness has been modified slightly to be compatible with TurboRav.
    
    The build system can generate random C programs with Csmith, build them
    for the host machine, and also build and simulate them on TurboRav.
    
    Next up is;
    
    Support for timeout of Csmith programs.
    
    Support for checking that TurboRav computes the same as the host
    machine (or maybe spike if the C programs are architecture-dependent).
    
    I have simulated 16 programs so far, the fifth program found a bug in
    the interaction between the linker script and the startup assembly code.
    To simulate more programs, I am going to need automatic checking of the
    C programs results.

---
+++ hw/src/test/csmith/csmith_minimal.h
@@ -0,0 +1,146 @@
+/* -*- mode: C -*-
+ *
+ * Copyright (c) 2007-2011, 2013, 2014 The University of Utah
+ * All rights reserved.
+ *
+ * This file is part of `csmith', a random generator of C programs.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef NO_PRINTF
+int putchar (int);
+#else
+extern int printf (const char *, ...);
+#endif
+
+// FIXME-- need more versions, and a way to figure out which is needed
+#include "custom_stdint_x86.h"
+
+#include "custom_limits.h"
+
+#define STATIC static
+
+#define UNDEFINED(__val) (__val)
+
+#define LOG_INDEX
+
+#define LOG_EXEC
+
+#define FUNC_NAME(x) (safe_##x)
+
+// FIXME
+#define assert(x)
+
+#include "safe_math.h"
+
+static inline void platform_main_begin(void)
+{
+}
+
+static inline void crc32_gentab (void)
+{
+}
+
+#define _CSMITH_BITFIELD(x) ((x>32)?(x%32):x)
+
+uint64_t crc32_context;
+
+#ifdef TCC
+int strcmp (const char *s1, const char *s2)
+{
+  for(; *s1 == *s2; ++s1, ++s2)
+    if(*s1 == 0)
+      return 0;
+  return *(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1;
+}
+#else
+extern int strcmp (const char *, const char *);
+#endif
+
+static inline void 
+transparent_crc (uint64_t val, char* vname, int flag)
+{
+#ifndef NO_PRINTF
+  if (flag) printf ("%s %d\n", vname, val);
+#endif
+  crc32_context += val;
+}
+
+static void 
+transparent_crc_bytes (char *ptr, int nbytes, char* vname, int flag)
+{
+  int i;
+  for (i=0; i<nbytes; i++) {
+    crc32_context += ptr[i];
+  }
+  if (flag) {
+    printf("...checksum after hashing %s : %lX\n", vname, crc32_context ^ 0xFFFFFFFFUL);
+  }
+}
+
+#ifdef NO_PRINTF
+void my_puts (char *p)
+{
+  write(0, p, strlen(p));
+}
+
+void put_hex (int x)
+{
+    char * tokens = "0123456789abcdef";
+    char * token_ptr = tokens + x;
+    write(0, token_ptr, 1);
+}
+#endif
+
+static inline void
+platform_main_end (uint64_t x, int flag)
+{
+#ifndef NOT_PRINT_CHECKSUM
+  if (!flag) {
+#ifdef NO_PRINTF
+    int i;
+    my_puts ("checksum = ");
+    for (i=0; i<16; i++) {
+      put_hex (x & 0xf);
+      x >>= 4;
+    }
+#else
+    printf ("checksum = %llx\n", x);
+#endif
+  }
+#endif
+
+#ifdef RISCV
+    asm volatile ("nop");
+    asm volatile ("nop");
+    asm volatile ("nop");
+    asm volatile ("nop");
+    asm volatile ("nop");
+    asm volatile ("nop");
+    asm volatile ("csrwi	tohost, 1");
+
+ infinite_loop:
+    goto infinite_loop;
+#endif
+}
