09d6a8e435aa89c180ab2381d695812d6dc1b787
Sebastian Bøe <sebastianbooe@gmail.com>
2016-05-06 16:49:50 +0200

    system_software: board: Renamed functions

--- hw/src/main/sw/system_software/board.c
+++ hw/src/main/sw/system_software/board.c
@@ -1,21 +1,21 @@
 #include <stdbool.h>
 #include "tr.h"
 
-bool board_get_btn2(void){
+bool board_btn_2_get(void){
     return GPIO0->input[0];
 }
 
-bool board_get_led1(void){
+bool board_led_1_get(void){
     return GPIO0->output[1];
 }
-bool board_get_led2(void){
+bool board_led_2_get(void){
     return GPIO0->output[2];
 }
 
-void board_set_led1(bool high){
+void board_led_1_set(bool high){
     GPIO0->output[1] = high;
 }
 
-void board_set_led2(bool high){
+void board_led_2_set(bool high){
     GPIO0->output[2] = high;
 }
--- hw/src/main/sw/system_software/board.h
+++ hw/src/main/sw/system_software/board.h
@@ -5,10 +5,10 @@
 // The board module extracts away some board-specific details so SW
 // can be written to be portable across dev-kits
 
-bool board_get_btn2(void);
+bool board_btn_2_get(void);
 
-bool board_get_led1(void);
-bool board_get_led2(void);
+bool board_led_1_get(void);
+bool board_led_2_get(void);
 
-void board_set_led1(bool);
-void board_set_led2(bool);
+void board_led_1_set(bool);
+void board_led_2_set(bool);
