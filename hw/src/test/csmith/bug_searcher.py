6e8c92b4fb52329777f53ba6cc4b945bc4c44955
Sebastian Bøe <sebastianbooe@gmail.com>
2016-05-19 21:25:25 +0200

    bug_searcher: skip tests that fail on the host

--- hw/src/test/csmith/bug_searcher.py
+++ hw/src/test/csmith/bug_searcher.py
@@ -5,18 +5,31 @@
 
 from subprocess import call
 import os
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description='Search for bugs with Csmith')
+    parser.add_argument(
+        '--start-seed',
+        default = 1
+    )
+
+    args = parser.parse_args()
+
     # This script assumes it is located and run from src/test/csmith
     os.chdir("../../../build/test/csmith/")
-    for seed in range(10000):
-        return_code = call(
-            [
-                "scons", "-u",
-                "csmith_seed={}".format(seed),
-                str(seed)
-            ]
-        )
+    for seed in range(int(args.start_seed), 10000):
+        scons_cmd = [
+            "scons", "-u",
+            "csmith_seed={}".format(seed)
+        ]
+
+        return_code = call( scons_cmd + ["{}/host_stdout.txt".format(seed)] )
+        if return_code:
+            print("Running on the host machine failed, so this seed is unusable")
+            continue
+
+        return_code = call( scons_cmd + ["{}".format(seed)] )
         assert(return_code == 0)
 
         os.chdir(str(seed))
